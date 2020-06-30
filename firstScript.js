2b172a4a00716db05609fd7e3e05e9b5c59eed0d
edgeos <edgeos@DESKTOP-M2EUKQ8>
2020-06-30 15:34:04 -0500

    Cambios al branch donde se adicionaron dos lineas de código

--- firstScript.js
+++ firstScript.js
@@ -1,2 +1,4 @@
 // JavaScript source code
-console.log("Hello World\n")
+console.log("Esto es el Cambio para hacer un branch\n");
+console.log("Esto es un branch todavia\n ");
+console.log("Pero sera un master proximamente\n ");
